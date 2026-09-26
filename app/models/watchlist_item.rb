b05d1ffd4b13cea8d4c4bb761ea62dcbda55a4d9
eric-camini482 <ericcamini482@gmail.com>
2026-09-26 10:44:42 -0300

    prevenção de duplicações e auto-follow

--- app/models/watchlist_item.rb
+++ app/models/watchlist_item.rb
@@ -1,4 +1,6 @@
 class WatchlistItem < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  validates :user_id, uniqueness: { scope: :movie_id, message: "já adicionou este filme à watchlist" }
 end
