9293d78d29bee4bb5290d5939a0b9671b474ddee
eric-camini482 <ericcamini482@gmail.com>
2026-09-26 10:55:18 -0300

    Validação, limitação e sanitização de dados

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,9 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
 
+  validates :body, length: { maximum: 2000 }
+
   validates :rating, numericality: { only_integer: true, in: 1..5 }, allow_nil: true
+
+  validates :user_id, uniqueness: { scope: :movie_id, message: "já avaliou este filme" }
 end
